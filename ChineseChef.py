09cd1402c313b744d1d84beb380852dca2ab61c7
SL0TR <mohaiminislam94@gmail.com>
2018-10-13 13:49:54 +0600

    add object, object functions

---
+++ ChineseChef.py
@@ -0,0 +1,9 @@
+from Chef import Chef
+
+class ChineseChef(Chef):
+    def make_special_dish(self):
+        print("The chef makes orange chicken")
+
+    def make_fried_chicken(selfs):
+        print("The chef makes fried chicken")
+
--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from Student import Student
+from Chef import Chef
+from ChineseChef import ChineseChef
 
 student1 = Student("Jim", "Business", 3.1, False)
 student2 = Student("Brad", "CSE", 3.5, False)
 student3 = Student("Lisa", "CSE", 1.5, True)
 
-print(student1.name)
+print(student2.on_honor_roll())
+
+myChef = Chef()
+myChef.make_special_dish()
+
+myChineseChef = ChineseChef()
+myChineseChef.make_special_dish()
+
